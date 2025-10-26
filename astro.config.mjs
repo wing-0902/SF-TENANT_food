64bddae270d8ca7019f38b69c0135f3bb3c95d96
Tsubasa <tsubasa@me.wing.osaka>
2025-10-26 21:04:52 +0900

    first

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [vue(), svelte()]
+});
