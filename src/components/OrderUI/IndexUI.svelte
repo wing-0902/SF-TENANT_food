90142e1aad3fa11564a0cd6ddbdcbb9997a02f3f
Wing <114337707+wing-0902@users.noreply.github.com>
2025-10-28 10:52:41 +0000

    order

--- src/components/OrderUI/IndexUI.svelte
+++ src/components/OrderUI/IndexUI.svelte
@@ -11,12 +11,13 @@
     userEmail = user.email;
     const メールアドレスによるユーザー属性: string = メールアドレスを分割(userEmail);
     userRole = ロールを判定(メールアドレスによるユーザー属性);
-  } else {
-    window.location.href = '/login/';
   }
 </script>
 
 <div>
   <p>ログインID：「{userEmail}」</p>
   <p>ロール：{userRole}</p>
+  {#if (userRole === "admin")}
+    <p>オーダを受け付けるには<a href='/order/'>こちら</a></p>
+  {/if}
 </div>
