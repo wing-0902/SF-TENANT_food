6b13e10fc933ca0e29df1476989d6f16a5c13360
Wing <114337707+wing-0902@users.noreply.github.com>
2025-10-28 10:45:36 +0000

    Role

---
+++ src/components/OrderUI/IndexUI.svelte
@@ -0,0 +1,22 @@
+<Rolscript lang='ts'>
+  import { getAuth } from "firebase/auth";
+  import { メールアドレスを分割, ロールを判定 } from "./splitEmailAddress.mts";
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+  let userEmail: string = '';
+  let userRole: string = '';
+
+  if ( user !== null && user.email !== null) {
+    userEmail = user.email;
+    const メールアドレスによるユーザー属性: string = メールアドレスを分割(userEmail);
+    userRole = ロールを判定(メールアドレスによるユーザー属性);
+  } else {
+    window.location.href = '/login/';
+  }
+</script>
+
+<div>
+  <p>ログインID：「{userEmail}」</p>
+  <p>ロール：{userRole}</p>
+</div>
---
+++ src/components/OrderUI/splitEmailAddress.mts
@@ -0,0 +1,19 @@
+const allowedAdminUser: string[] = [
+  'tsubasa',
+  'admin',
+  'guest',
+  'test',
+]
+
+export function メールアドレスを分割(address: string): string {
+  const parts = address.split('@');
+  return parts[0];
+};
+
+export function ロールを判定(userNameFromMail: string): string {
+  if ( allowedAdminUser.includes(userNameFromMail)) {
+    return 'admin';
+  } else {
+    return 'userNameFromMail';
+  }
+};
